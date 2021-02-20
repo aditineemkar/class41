c602c2ffb7fa0a4878dac14842069b07c9ac2e15
Aditi <sneemkar@gmail.com>
2021-02-20 16:23:23 +0530

    multiplayerracinggame

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -92,6 +92,8 @@ class Game {
     }
 
     if(player.distance > 3860){
+      player.rank+=1
+      Player.updateCarsAtEnd(player.rank);
       gameState = 2;
     }
    
@@ -100,5 +102,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -30,6 +31,16 @@ class Player {
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
+    })
+  }
+  getCarsAtEnd(){
+    database.ref('carAtEnd').on("value",(data)=>{
+      this.rank=data.val();
+    })
+  }
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      carAtEnd:rank
     })
   }
 }
